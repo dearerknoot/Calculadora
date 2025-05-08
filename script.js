b0e3bfafc61be008d5d011f8b97ebc079488bc09
dearerknoot <thiagocarlesso08@gmail.com>
2025-05-08 20:31:40 -0300

    calculadora finalizada

--- script.js
+++ script.js
@@ -1,92 +1,75 @@
 let numeros = document.querySelectorAll('.box .numero');
-const numero1Content = document.getElementById('valor1');
-const numero2Content = document.getElementById('valor2');
-const opContent = document.getElementById('op');
-const resultado = document.getElementById('total');
+let operacoes = document.querySelectorAll('.box .op');
+const resultado = document.getElementById('resultado');
 let contador = 1;
-let validar = false;
+let validarTotal = false;
+let validarOp = true;
 let op = '';
-let v1 = 0 , v2 = 0;
-console.log(contador);
-numeros.forEach(numero =>{
-    numero.addEventListener('click' , ()=>{
-        if(contador == 1){
-            console.log(contador);
-            let n1 = numero.dataset.value;
-            if(n1 == '+' || n1 == '-' || n1 == '=' || n1 == '/' || n1 == 'x'){
-               alert('valor inválido');
+let n1 = '', n2 = '';
+if(validarOp){
+    operacoes.forEach(operacao =>{
+        operacao.addEventListener('click',()=>{
+            if(n1 == ''){
+                alert('Número inválido')
             }else{
-                numero1Content.textContent = n1;
+                op = operacao.dataset.value;
                 contador++;
-                v1 = parseInt(n1)
+                resultado.textContent = op;
             }
-        }else if(contador == 2){
-            let operador = numero.dataset.value;
-            console.log(contador);
-            console.log(operador)
-            if(operador == '+' || operador == '-' || operador == 'x' || operador == '/'){
-                contador++;
-                opContent.textContent = operador;
-                if(operador == '+'){
-                    op = '+';
-                }else if(operador == '-'){
-                    op = '-';
-                }else if(operador == 'x'){
-                    op = 'x';
-                }else if(operador == '/'){
-                    op = '/';
-                }
-            }else{
-                alert('operação inválida');
+        })
+    })
+}
+
+    numeros.forEach(numero =>{
+        numero.addEventListener('click',()=>{
+            if(contador == 1){
+                let valor = numero.dataset.value;
+                n1 = n1 + valor;
+                console.log(n1)
+                validarOp = true;
+                console.log(validarOp)
+                resultado.textContent = n1;
+            }else if(contador == 2){
+                let valor = numero.dataset.value;
+                n2 = n2 + valor;
+                console.log(n2)
+                resultado.textContent = n2;
+                validarTotal = true
             }
-        }else if(contador == 3){
-            let n2 = numero.dataset.value;
-            if(n2 == '+' || n2 == '-' || n2 == '=' || n2 == '/' || n2 == 'x'){
-                alert('valor inválido');
-            }else{
-                contador++;
-                numero2Content.textContent = n2;
-                validar = true;
-                v2 = parseInt(n2);
-            }
-        }
-    });
-});
+        })
+    })     
+    
+    
+
 function calcular(){
-    if(validar){
+    if(validarTotal){
         if(op == '+'){
-            let soma = v1 + v2;
-            numero1Content.textContent = '';
-            numero2Content.textContent = '';
-            opContent.textContent = '';
+            let soma = parseInt(n1) + parseInt(n2);
             resultado.textContent = soma;
         }else if(op == '-'){
-            let sub = v1 - v2;
-            numero1Content.textContent = '';
-            numero2Content.textContent = '';
-            opContent.textContent = '';
+            let sub = parseInt(n1) - parseInt(n2);
             resultado.textContent = sub;
         }else if(op == 'x'){
-            let mult = v1 * v2;
-            numero1Content.textContent = '';
-            numero2Content.textContent = '';
-            opContent.textContent = '';
+            let mult = parseInt(n1) * parseInt(n2);
             resultado.textContent = mult;
         }else if(op == '/'){
-            let div = v1 / v2;
-            numero1Content.textContent = '';
-            numero2Content.textContent = '';
-            opContent.textContent = '';
+            let div = parseInt(n1) / parseInt(n2);
             resultado.textContent = div;
-            // let divString = div.toString();
-            // let partes = divString.split('.');
-            // if(partes[1].length > 4){
-            //     resultado.textContent = div.toFixed(4);
-            // }else{
-               
-            // }
+            let divString = div.toString();
+            let partes = divString.split('.');
+            if(partes[1].length > 4){
+                resultado.textContent = div.toFixed(4);
+            }
         }
     }else{
         
     }
+}
+function del(){
+    resultado.textContent = ''
+    contador = 1;
+    validarTotal = false;
+    validarOp = true;
+    op = '';
+    n1 = '', n2 = '';
 }
